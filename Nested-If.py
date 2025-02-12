6ea0d0e7a229e90bf4519159a71aadf97eece4be
DivyaSiddam <166330116+DivyaSiddam@users.noreply.github.com>
2025-02-12 00:14:57 -0500

    Create Nested-If.py

---
+++ Nested-If.py
@@ -0,0 +1,7 @@
+print("welcome to rollcoaster!")
+height=int(input("what is your height in cm?")
+if height > = 120:
+   print("can ride a rollcoaster")
+else:
+  print("sorry you need to grow")
+               
