d62c782f15bd84af2540c49549a6246523a2b3a8
Divya siddam <166330116+DivyaSiddam@users.noreply.github.com>
2025-02-13 22:45:30 -0500

    Create TODO-1.py

---
+++ TODO-1.py
@@ -0,0 +1,13 @@
+import random
+word_list = ["aardvark", "baboon", "camel"]
+
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
+print(placeholder)
+
+
