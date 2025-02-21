23a494f636ec9f0b28c39102b6dd3ecace630b94
Divya siddam <166330116+DivyaSiddam@users.noreply.github.com>
2025-02-20 21:26:09 -0500

    Debugging Odd or Even.py

---
+++ Debugging Odd or Even.py
@@ -0,0 +1,8 @@
+def odd_or_even(number):
+    if number % 2 = 0:
+        return "This is an even number."
+    else:
+        return "This is an odd number."
+
+  #you will get an error as "SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?"
+ to solve that you need to you need to add "==" at a place near if number%2==0:
