ded95a03fd233241f8839cbd9d2287a955676d11
Divya siddam <166330116+DivyaSiddam@users.noreply.github.com>
2025-02-24 18:12:37 -0500

    lists.py

---
+++ lists.py
@@ -0,0 +1,35 @@
+list1 = [2, 3, 4, 1, 32]
+len(list1)
+
+
+# min,max,sum
+min(list1)
+max(list1)
+sum(list1)
+
+
+
+
+
+#shuffle 
+import random
+random.shuffle(list1) # Shuffle the items in the list
+list1
+
+
+# Adding 2 lists
+list1 = [2, 3]
+list2 = [1, 9]
+list3 = list1 + list2
+list3
+# multiplying
+list3 = 2 * list1
+list3
+
+
+
+list4 = list3[2 : 4]
+list4
+
+
+
