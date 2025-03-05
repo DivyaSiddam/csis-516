f92b9c20948e1388074022405087237743ed958b
Divya siddam <166330116+DivyaSiddam@users.noreply.github.com>
2025-03-04 19:13:07 -0500

    Printing lists.py

---
+++ Printing lists.py
@@ -0,0 +1,5 @@
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] # Assume a list is given
+for row in range(0, len(matrix)):
+for column in range(0, len(matrix[row])):
+print(matrix[row][column], end = " ")
+print() # Print a newline
