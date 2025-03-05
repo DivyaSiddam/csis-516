4fa94e6568ca8f57063d93b7b68fe95e0c157958
Divya siddam <166330116+DivyaSiddam@users.noreply.github.com>
2025-03-04 19:14:52 -0500

    Summing all elements by column.py

---
+++ Summing all elements by column.py
@@ -0,0 +1,6 @@
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] # Assume a list is given
+total = 0
+for column in range(0, len(matrix[0])):
+for row in range(0, len(matrix)):
+total += matrix[row][column]
+print("Sum for column " + str(column) + " is " + str(total))
